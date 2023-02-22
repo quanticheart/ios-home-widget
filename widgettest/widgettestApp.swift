644c38c6dbb3218dfa61e75126e4e7d4bc6ea7fd
Jonn Alves <jonatas.alsantos@jsl.com.br>
2023-02-22 08:22:34 -0300

    Initial Commit

---
+++ widgettest/widgettestApp.swift
@@ -0,0 +1,17 @@
+//
+//  widgettestApp.swift
+//  widgettest
+//
+//  Created by Jonn Alves on 22/02/23.
+//
+
+import SwiftUI
+
+@main
+struct widgettestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
